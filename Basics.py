a4ca6882b2f7fce27e541718a6ed18605805f556
Dylan Scheidegg <62270254+DylanScheidegg@users.noreply.github.com>
2020-10-25 21:36:04 -0400

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -1,49 +1,72 @@
+# This is an import it takes functions from the directory specified to be used
 import random
 
-# This is a print statement
-print("hi")
+# This is a variable. They are the soul of programming. They are like a piece of paper that hold a set of data. They can usually hold an infinite amount of data
+# They can hold any data type available
+varExample = "var"
 
-# Integers
+# This is a print statement it writes to the output screen
+print("hi")
+# Print statements can both display hard coded text and variables
+print("Hardcoded", varExample)
+
+# Integers which is a normal number
 intNum = 1
 print("This is an int: ", intNum)
 
-# Doubles
+# Doubles which are decimal numbers
 doubleNum = 1.11
 print("This is a double: ", doubleNum)
 
-# Booleans
+# Strings which are just sentences
+stringWord = "Im a string"
+print(stringWord)
+
+# Booleans are either true or false. They are used in verifying if statements are true or not
 boolTF = True
 print("This is a boolean: ", boolTF)
 
 # If statement
 if boolTF:
+    # if the statement is meet then it does this
     print("The statement is true")
 else:
+    # if the statement is NOT meet then it does this
     print("The statement is false")
 
 # While Loop
+# Works by looping through its set of instructions until its
+# This is a counting variable. These are used everywhere. They are used to stop a section of code when a threshold is meet
 count = 0
 while boolTF:
+    # Incrementing the count by 1 
     count += 1
     if count == 3:
+        # When the counter is at 3 the code stops
         boolTF = False
     print("Counting while loop")
 
 # For Loop counting
+# This is a loop that keeps going but increments itself unlike the while loop that needs a little help with a variable
+# range() just hold a number that is the threshold
 for x in range(3):
     print(x)
 
 # For loop array display
+# This is an array that holds strings, it could be any data type
 arrNames = ["Dylan", "Dom", "Alex"]
+# this loops throught the arrays indexes and displays each value of the array in order
 for names in arrNames:
     print(names)
 
 # For Loop counting with array
 arrNames1 = ["Dylan", "Dom", "Alex"]
+# len() gives back the length/size of the array for the range()/threshold
 for x in range(len(arrNames1)):
+    # This gets the index and then displays the data from that index
     print(arrNames1[x])
 
-# User input
+# User input and puts it into a variable
 uName = input("Enter your name: ")
 print("Your name is: " + uName)
 
@@ -103,6 +126,51 @@ print(arrNest2d)
 size3d = 3
 arrNest3d = [[[x for x in range(size3d)] for y in range(size3d)] for z in range(size3d)]
 print(arrNest3d)
+
+# NESTED ARRAYS 4D
+# it will look like this 3 x 3 x 3 x 3
+# Index 0:
+    # Index 0:
+        # Index 0: |0, 1, 2|
+        # Index 1: |0, 1, 2|
+        # Index 2: |0, 1, 2|
+    # Index 1:
+        # Index 0: |0, 1, 2|
+        # Index 1: |0, 1, 2|
+        # Index 2: |0, 1, 2|
+    # Index 2:
+        # Index 0: |0, 1, 2|
+        # Index 1: |0, 1, 2|
+        # Index 2: |0, 1, 2|
+# Index 1:
+    # Index 0:
+        # Index 0: |0, 1, 2|
+        # Index 1: |0, 1, 2|
+        # Index 2: |0, 1, 2|
+    # Index 1:
+        # Index 0: |0, 1, 2|
+        # Index 1: |0, 1, 2|
+        # Index 2: |0, 1, 2|
+    # Index 2:
+        # Index 0: |0, 1, 2|
+        # Index 1: |0, 1, 2|
+        # Index 2: |0, 1, 2|
+# Index 2:
+    # Index 0:
+        # Index 0: |0, 1, 2|
+        # Index 1: |0, 1, 2|
+        # Index 2: |0, 1, 2|
+    # Index 1:
+        # Index 0: |0, 1, 2|
+        # Index 1: |0, 1, 2|
+        # Index 2: |0, 1, 2|
+    # Index 2:
+        # Index 0: |0, 1, 2|
+        # Index 1: |0, 1, 2|
+        # Index 2: |0, 1, 2|
+size4d = 3
+arrNest4d = [[[[x for x in range(size4d)] for y in range(size4d)] for z in range(size4d)] for w in range(size4d)]
+print(arrNest4d)
 
 # Reverse array
 print("Before: ", arrAdd)
